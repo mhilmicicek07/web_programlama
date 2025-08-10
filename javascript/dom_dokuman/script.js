ccb5cb9c3a67018e8e0636c634c748fb6a89a0a1
mhilmicicek07 <m.hilmicicek_07@icloud.com>
2025-08-10 21:13:01 +0200

    update

--- javascript/dom_dokuman/script.js
+++ javascript/dom_dokuman/script.js
@@ -327,10 +327,10 @@
 // btn.addEventListener("click",function(a){
 //     let value;
 //     value = a;
-    
+
 //     a.preventDefault(); // Delete Allbutonu tıklandığında sayfa yenilenmemesi için yazılan fonksiyon
 //     console.log(value); // PointerEvent {isTrusted: true, pointerId: 1, width: 1, height: 1, pressure: 0, …}
-    
+
 //     value = a.target;
 //     console.log(value); // Tıklanan buton -> <a id="btnDeleteAll" href="#" class="btn btn-outline-primary btn-sm delete-all float-right">Delete All</a>
 //     value = a.target.id;
@@ -493,153 +493,175 @@
 // const ul = document.querySelector("ul");
 
 // ul.addEventListener("click",function(e){
-//     // console.log(e.target)
+//     console.log(e.target)
 //     if (e.target.className === "fas fa-times") {
 //         e.target.parentElement.parentElement.remove();
 //     }
 // });
 
 
+//! ToDo Eleman Ekleme
+
+//? Eleman Seçimi
+
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnAddNewTask = document.querySelector("#btnAddNewTask");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+// const items = ["Todo 1", "Todo 2", "Todo 3", "Todo 4","Todo 5","Todo 6"];
+let todos;
 
 
+//? Load Items Fonskiyonu
+
+function loadItems() {
+    // items.forEach(function (item) {
+    //     createItem(item);
+    // });
+    todos = getItemsFromLS();
+    todos.forEach(function (item) {
+        createItem(item);
+    })
+}
+
+loadItems();
 
 
+eventListeners();
 
-
-
-// ToDo Eleman Ekleme
-
-// Eleman Seçimi
-
-// const form = document.querySelector("form");
-// const input = document.querySelector("#txtTaskName");
-// const btnAddNewTask = document.querySelector("#btnAddNewTask");
-// const btnDeleteAll = document.querySelector("#btnDeleteAll");
-// const taskList = document.querySelector("#task-list");
-// let todos;
-
-
-// Load Items Fonskiyonu
-// loadItems();
-
-
-// eventListeners();
-
-// function eventListeners() {
+function eventListeners() {
     // Submit Event
-//     form.addEventListener("submit", addNewItem);
+    // console.log("submit")
+    form.addEventListener("submit", addNewItem);
     // Delete an Item
-//     taskList.addEventListener("click", deleteItem);
+    taskList.addEventListener("click", deleteItem);
     // Delete all Item
-//     btnDeleteAll.addEventListener("click", deleteItems);
+    btnDeleteAll.addEventListener("click", deleteAllItems);
+}
 
-// }
+//? Get Items From Local Storage
 
-// function loadItems() {
-//     todos = getItemsFromLS();
-//     todos.forEach(function (item) {
-//         createItem(item);
-//     })
+function getItemsFromLS() {
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    }
+    else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    return todos;
+}
 
-// }
+//? Set Item To Local Storage
 
+function setItemToLS(newTodo) {
+    todos = getItemsFromLS();
+    todos.push(newTodo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 
-// Get Items From Local Storage
-// function getItemsFromLS() {
-//     if (localStorage.getItem("todos") === null) {
-//         todos = [];
-//     } else {
-//         todos = JSON.parse(localStorage.getItem("todos"));
-//     }
-//     return todos;
-
-// }
-// Set Item To Local Storage
-// function setItemToLS(newTodo) {
-//     todos = getItemsFromLS();
-//     todos.push(newTodo);
-//     localStorage.setItem("todos", JSON.stringify(todos));
-
-// }
-
-// function createItem(newTodo) {
+function createItem(newTodo) {
     // li(liste elemanı) oluşturma
 
-//     const li = document.createElement("li");
-//     li.className = "list-group-item list-group-item-secondary";
-//     li.appendChild(document.createTextNode(newTodo));
+    const li = document.createElement("li");
+    li.className = "list-group-item list-group-item-secondary";
+    // li.appendChild(document.createTextNode(text));
+    li.appendChild(document.createTextNode(newTodo));
 
     // a(link) oluşturmak
 
-//     const a = document.createElement("a");
-//     a.classList = "delete-item float-right";
-//     a.setAttribute("href", "#");
-//     a.innerHTML = '<i class="fas fa-times"></i>';
+    const a = document.createElement("a");
+    a.classList = "delete-item float-right";
+    a.setAttribute("href", "#");
+    a.innerHTML = '<i class="fas fa-times"></i>';
 
     // Birbiriyle Bağlantılama 
 
-//     li.appendChild(a);
+    li.appendChild(a);
 
-//     taskList.appendChild(li);
-// }
+    taskList.appendChild(li);
+}
 
-// function addNewItem(e) {
-//     if (input.value === '') {
-//         alert("add new item");
+function addNewItem(e) {
+    // console.log("submit")
+
+    if (input.value === '') {
+        alert("add new item");
         // console.log("submit");
-//     }
+    }
 
-    // Create Item
+    // // li oluşturma
 
-//     createItem(input.value);
+    // const li = document.createElement("li");
+    // li.className = "list-group-item list-group-item-secondary";
+    // li.appendChild(document.createTextNode(input.value));
 
-//     setItemToLS(input.value);
+    // // a oluşturma
 
+    // const a = document.createElement("a");
+    // a.classList = "delete-item float-right";
+    // a.setAttribute("href", "#");
+    // a.innerHTML = '<i class="fas fa-times"></i>';
 
-//     input.value = "";
+    // li.appendChild(a);
+    // taskList.appendChild(li);
 
-//     e.preventDefault();
+    //* Create Item
 
-// }
+    createItem(input.value);
 
-// Eleman Silme
+    setItemToLS(input.value);
 
-// function deleteItem(e) {
+    input.value = "";
 
+    e.preventDefault();
 
-//     if (e.target.className === "fas fa-times") {
-//         if (confirm("Silmek İstediğinize Emin Misiniz?")) {
+}
+
+//? Eleman Silme
+
+function deleteItem(e) {
+
+    // console.log(e.target)
+
+    if (e.target.className === "fas fa-times") {
+        if (confirm("Silmek İstediğinize Emin Misiniz?")) {
             // console.log(e.target);
-//             e.target.parentElement.parentElement.remove();
-//             deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
-//         }
-//     }
-//     e.preventDefault();
+            e.target.parentElement.parentElement.remove();
+            deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        }
+    }
+    e.preventDefault();
+}
 
-// }
+//? Local Storage dan eleman silme
 
-// function deleteTodoFromStorage(deletetodo) {
-//     let todos = getItemsFromLS();
+function deleteTodoFromStorage(deletetodo) {
+    let todos = getItemsFromLS();
 
-//     todos.forEach(function (todo, index) {
-//         if (todo === deletetodo) {
-//             todos.splice(index, 1);
-//         }
-//     });
-//     localStorage.setItem("todos", JSON.stringify(todos));
+    todos.forEach(function (todo, index) {
+        if (todo === deletetodo) {
+            todos.splice(index, 1);
+        }
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 
-// }
+//? Tüm Elemanları Silme
 
-// Tüm Elemanları Silme
+function deleteAllItems(e) {
+    if (confirm("Tüm Elemanları Silmek İstediğinize Emin Misiniz?")) {
 
-// function deleteItems(e) {
-//     if (confirm("Tüm Elemanlari Silmek İstediğinize Emin Misiniz?")) {
-
-//         while (taskList.firstChild) {
-//             taskList.removeChild(taskList.firstChild);
-//         }
-//         localStorage.clear();
-
-//     }
+        // taskList.childNodes.forEach(function (item) {
+        //     console.log(item)
+        //     if (item.nodeType == 1) {
+        //         item.remove();
+        //     }
+        // });
+        while (taskList.firstChild) {
+            taskList.removeChild(taskList.firstChild);
+        }
+        localStorage.clear();
+    }
     // taskList.innerHTML=""; // Alternatif Yöntem
-// }
+}
